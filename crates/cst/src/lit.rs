228255002eb57ea35f48dab7c6de2994b69abe81
yayanyang <yayanyang@gmail.com>
2025-11-21 19:00:12 +0800

    update `unit` parser

--- crates/cst/src/lit.rs
+++ crates/cst/src/lit.rs
@@ -110,6 +110,46 @@ where
     }
 }
 
+/// Unit of literal numbers.
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Syntax)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub enum Unit<I>
+where
+    I: CSTInput,
+{
+    I8(#[parserc(keyword = "i8")] I),
+    I16(#[parserc(keyword = "i16")] I),
+    I32(#[parserc(keyword = "i32")] I),
+    I64(#[parserc(keyword = "i64")] I),
+    I128(#[parserc(keyword = "i128")] I),
+    I256(#[parserc(keyword = "i256")] I),
+    U8(#[parserc(keyword = "u8")] I),
+    U16(#[parserc(keyword = "u16")] I),
+    U32(#[parserc(keyword = "u32")] I),
+    U64(#[parserc(keyword = "u64")] I),
+    U128(#[parserc(keyword = "u128")] I),
+    U256(#[parserc(keyword = "u256")] I),
+    F16(#[parserc(keyword = "f16")] I),
+    F32(#[parserc(keyword = "f32")] I),
+    F64(#[parserc(keyword = "f64")] I),
+    Nanosecond(#[parserc(keyword = "ns")] I),
+    Microsecond(#[parserc(keyword = "us")] I),
+    Millisecond(#[parserc(keyword = "ms")] I),
+    Second(#[parserc(keyword = "s")] I),
+    Minute(#[parserc(keyword = "mins")] I),
+    Hour(#[parserc(keyword = "h")] I),
+    Day(#[parserc(keyword = "d")] I),
+    Em(#[parserc(keyword = "em")] I),
+    Ex(#[parserc(keyword = "ex")] I),
+    Px(#[parserc(keyword = "px")] I),
+    In(#[parserc(keyword = "in")] I),
+    Cm(#[parserc(keyword = "cm")] I),
+    Mm(#[parserc(keyword = "mm")] I),
+    Pt(#[parserc(keyword = "pt")] I),
+    Pc(#[parserc(keyword = "pc")] I),
+    Percent(#[parserc(keyword = "%")] I),
+}
+
 #[cfg(test)]
 mod tests {
     use parserc::syntax::InputSyntaxExt;
@@ -117,6 +157,53 @@ mod tests {
     use crate::TokenStream;
 
     use super::*;
+
+    #[test]
+    fn test_unit() {
+        macro_rules! check_unit {
+            ($ident: ident, $input: literal, $match: literal) => {
+                assert_eq!(
+                    TokenStream::from($input).parse(),
+                    Ok(Unit::$ident(TokenStream::from($match))),
+                );
+            };
+        }
+
+        check_unit!(I8, "i8~", "i8");
+        check_unit!(I16, "i16~", "i16");
+        check_unit!(I32, "i32~", "i32");
+        check_unit!(I64, "i64~", "i64");
+        check_unit!(I128, "i128~", "i128");
+        check_unit!(I256, "i256~", "i256");
+
+        check_unit!(U8, "u8~", "u8");
+        check_unit!(U16, "u16~", "u16");
+        check_unit!(U32, "u32~", "u32");
+        check_unit!(U64, "u64~", "u64");
+        check_unit!(U128, "u128~", "u128");
+        check_unit!(U256, "u256~", "u256");
+
+        check_unit!(F16, "f16~", "f16");
+        check_unit!(F32, "f32~", "f32");
+        check_unit!(F64, "f64~", "f64");
+
+        check_unit!(Nanosecond, "ns~", "ns");
+        check_unit!(Microsecond, "us~", "us");
+        check_unit!(Millisecond, "ms~", "ms");
+        check_unit!(Second, "s~", "s");
+        check_unit!(Minute, "minss", "mins");
+        check_unit!(Hour, "hrs", "h");
+        check_unit!(Day, "day", "d");
+        check_unit!(Em, "em~", "em");
+        check_unit!(Ex, "ex", "ex");
+        check_unit!(Px, "px", "px");
+        check_unit!(In, "in~", "in");
+        check_unit!(Cm, "cm", "cm");
+        check_unit!(Mm, "mm~", "mm");
+        check_unit!(Pt, "pt", "pt");
+        check_unit!(Pc, "pc", "pc");
+        check_unit!(Percent, "%~", "%");
+    }
 
     #[test]
     fn test_lit_str() {
